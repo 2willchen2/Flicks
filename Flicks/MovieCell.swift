179ec45968d6e0c4d28d26fff5b33e19a76f449f
william chen <Will@williams-air.mobile.asu.edu>
2016-01-29 15:09:57 -0700

    Added the first part of the flicks app

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by william chen on 1/22/16.
+//  Copyright © 2016 william chen. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+     
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+ 
